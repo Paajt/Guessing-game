6f0e7f26b74c18dbd9e8766f23180943f88d1967
Patrik <pat_p3@hotmail.com>
2025-04-03 14:42:31 +0200

    Basic styling and font for boilerplate start page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,18 @@ import Button from "react-bootstrap/Button";
 
 export default function App() {
   return (
-    <div>
-      <h1>Testing</h1>
-      <Button variant="primary">Primary</Button>
-      <Button variant="secondary">Secondary</Button>
+    <div className="Bg">
+      <div className="content">
+        <h1>Wordle look-alike</h1>
+        <h2>Welcome!</h2>
+        <p>This is a word guessing game.</p>
+        <p>Click 'Play game' to try it out!</p>
+      </div>
+      <div className="buttons">
+        <Button variant="info">Info</Button>
+        <Button variant="success">Play game</Button>
+        <Button variant="warning">Highscore</Button>
+      </div>
     </div>
   );
 }
