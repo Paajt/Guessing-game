30432f381065a2a97031060bdf09144349104533
Patrik <pat_p3@hotmail.com>
2025-04-03 13:21:22 +0200

    Installed Vite, React, Bootstrap for frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import Button from "react-bootstrap/Button";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Testing</h1>
+      <Button variant="primary">Primary</Button>
+      <Button variant="secondary">Secondary</Button>
+    </div>
+  );
+}
