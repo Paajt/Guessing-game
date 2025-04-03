1c3ebbcb5b136a6f2c7d4fbdd9e9d102d28e0f4d
Patrik <pat_p3@hotmail.com>
2025-04-03 21:48:43 +0200

    startGame() - choose game settings before playing

--- frontend/src/components/Game/Game.jsx
+++ frontend/src/components/Game/Game.jsx
@@ -1,25 +1,63 @@
+import "./game.css";
+import Button from "react-bootstrap/Button";
 import { useState } from "react";
 
 export default function Game() {
-  const [guess, setGuess] = useState("");
+  // const [guess, setGuess] = useState("");
+  const [gameStarted, setGameStarted] = useState(false);
+  const [wordLength, setWordLength] = useState(4);
+  const [allowDuplicates, setAllowDuplicates] = useState(true);
 
-  function handleSubmit(e) {
+  function startGame(e) {
     e.preventDefault();
-    console.log("Du gissade:", guess);
-    setGuess("");
+    console.log("Game settings:");
+    console.log("Letters:", wordLength);
+    console.log("Duplicates?", allowDuplicates);
+    setGameStarted(true);
   }
+
+  // function handleSubmit(e) {
+  //   e.preventDefault();
+  //   console.log("Du gissade:", guess);
+  //   setGuess("");
+  // }
 
   return (
     <div>
-      <h2>Gissa ordet</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={guess}
-          onChange={(e) => setGuess(e.target.value)}
-        />
-        <button type="submit">Gissa</button>
-      </form>
+      {!gameStarted ? (
+        <form onSubmit={startGame}>
+          <h2>Choose Settings</h2>
+
+          <label>
+            Amount of letters in word: {wordLength}
+            <input
+              type="range"
+              min="3"
+              max="8"
+              value={wordLength}
+              onChange={(e) => setWordLength(Number(e.target.value))}
+            />
+          </label>
+
+          <br />
+
+          <label>
+            Allow duplicate letters?
+            <input
+              type="checkbox"
+              checked={allowDuplicates}
+              onChange={(e) => setAllowDuplicates(e.target.checked)}
+            />
+          </label>
+
+          <br />
+          <Button variant="success" type="submit">
+            Start game
+          </Button>
+        </form>
+      ) : (
+        <p>Game below here</p>
+      )}
     </div>
   );
 }
