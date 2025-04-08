589088499e508a829b146f0d4051936d3d476793
Patrik <pat_p3@hotmail.com>
2025-04-08 13:55:11 +0200

    Installed mongoose and setup connection to MongoDB

---
+++ backend/db/connect.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export async function connectToDatabase() {
+  try {
+    await mongoose.connect("mongodb://localhost:27017/wordle-game");
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error("MongoDB connection error!", err);
+    process.exit(1);
+  }
+}
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 import express from "express";
 import wordRoutes from "./routes/wordRoutes.js";
+import { connectToDatabase } from "./db/connect.js";
 
 const app = express();
 const PORT = 5080;
 
 app.use("/api/word", wordRoutes);
+
+await connectToDatabase();
 
 app.listen(PORT, () => {
   console.log(`Backend is running on localhost:${PORT}`);
